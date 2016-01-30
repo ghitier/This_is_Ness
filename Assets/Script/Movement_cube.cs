30c7186819e2211820640b16629004f31562d986
Didier-Gerschheimer <didier1.gerschheimer@epitech.eu>
2016-01-30 17:07:12 +0100

    Work : Boite qui bouge

---
+++ Assets/Script/Movement_cube.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement_cube : MonoBehaviour {
+
+    public float[] spawn_coords = new float[] { -1.98f, 0.00f};
+    public float coords = 0.0f;
+    public int direction = 0;
+
+	void Update ()
+    {
+        if (direction == 0)
+        { 
+            transform.position = new Vector3(spawn_coords[0] + coords, spawn_coords[1], 0);
+            coords += 0.015f;
+            if (coords >= 4.2f)
+            {
+                direction++;
+                coords = 0;
+            }
+        }
+        if (direction == 1)
+        {
+            transform.position = new Vector3(spawn_coords[0] + 4.2f, spawn_coords[1] - coords, 0);
+            coords = coords + 0.015f;
+            if (coords >= 2.43f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
