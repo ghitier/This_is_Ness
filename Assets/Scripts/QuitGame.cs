04a5217aecbcd4b0fc4313e66bc690ec03196f72
Nicolas Di Nunno <github@ndinunno.fr>
2016-01-30 21:10:37 +0100

    New Repo data

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+    void OnMouseDown() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+        void OnMouseDown() {
+            SceneManager.LoadScene("house");
+    }
+}
