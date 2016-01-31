5bf5546f26aab4502589bfad8bbebc1fcd261881
hitier_g <guillaume.hitier@epitech.eu>
2016-01-31 15:28:40 +0100

    door fix

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Door : MonoBehaviour {
+
+    void    OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.name == "Character")
+            SceneManager.LoadScene("Work");
+    }
 
 	// Use this for initialization
 	void Start () {
